3a2fbe4c8642e0526194071f31363bf7328b69ed
106_Sam <70407632+106-Sam@users.noreply.github.com>
2022-10-03 12:27:16 +0530

    Update forms.py

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class TodoListForm(forms.Form):
     text = forms.CharField(max_length=45,
             widget=forms.TextInput(
-                attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g Happiness Khushii', 'aria-label' : 'Todo', 'aria-describeby' : 'add-btn'}))
+                attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g Hack The Planet', 'aria-label' : 'Todo', 'aria-describeby' : 'add-btn'}))
